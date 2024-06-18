266a4ced96f25fb711c9a752ce11eeaa93c354f1
user_komeda <shigoto922@gmail.com>
2024-06-18 22:27:07 +0900

    #2 index route 作成

---
+++ src/typs/itemsData.d.ts
@@ -0,0 +1,12 @@
+export interface ItemsData {
+  body: string;
+  id: string;
+  likes_count: number;
+  private: boolean;
+  reactions_count: number;
+  stocks_count: number;
+  tags: Array<string>;
+  title: string;
+  url: string;
+  page_views_count: number;
+}
