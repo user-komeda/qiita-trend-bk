df36dd72072c7bceb37c51dde5a7633346520c62
user_komeda <shigoto922@gmail.com>
2024-06-21 19:06:30 +0900

    #10 コメント取得

---
+++ src/app/comment/infrastructure/comment.repositoryImpl.ts
@@ -0,0 +1,28 @@
+import { HttpService } from '@nestjs/axios';
+import { Injectable } from '@nestjs/common';
+import { CommentRepository } from '../domain/comment.repository';
+import { lastValueFrom, map } from 'rxjs';
+
+@Injectable()
+export class CommentRepositoryImpl implements CommentRepository {
+  constructor(private readonly httpService: HttpService) {}
+  async getItemComment(id: string): Promise<string[]> {
+    return await lastValueFrom(
+      this.httpService.get(this.buildUrl(id)).pipe(
+        map((response) => {
+          return this.convertData(response.data);
+        }),
+      ),
+    );
+  }
+
+  private buildUrl(id: string): string {
+    return `https://qiita.com/api/v2/items/${id}/comments`;
+  }
+
+  private convertData(dataList): Array<string> {
+    return dataList.map((data) => {
+      return data.body;
+    });
+  }
+}
